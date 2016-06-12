f2a3b7f04f05b3eb48899c1bf1b592f1cb95d799
qlevaslo <quentin.levaslot@capgemini.com>
2016-06-12 10:12:35 +0200

    feat(configuration): Add eslint and upgrade to gulp 4.0

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,12 @@ var gulp = require('gulp');
 var gulpAutoprefixer = require('gulp-autoprefixer');
 var browserSync = require('browser-sync');
 var gulpConcat = require('gulp-concat');
-var gulpEslint = require('gulp-eslint');
 var gulpSass = require('gulp-sass');
 var gulpSourcemaps = require('gulp-sourcemaps');
 
 var babelify = require('babelify');
 var browserify = require('browserify');
+var eslintify = require('eslintify');
 var reactify = require('reactify');
 var vinylSourceStream = require('vinyl-source-stream');
 var vinylBuffer = require('vinyl-buffer');
@@ -16,81 +16,81 @@ var watchify = require('watchify');
 
 // Build javascript files with browserify, reactify and babelify
 function compile(watch) {
-    var plugin = [];
-    if (watch) {
-        plugin = [[watchify, {ignoreWatch: ['**/node_modules/**']}]]
-    }
-    var bundler = browserify({
-        entries: "./app/index.jsx",
-        debug: true,
-        cache: {},
-        packageCache: {},
-        plugin: plugin,
-        transform: [["babelify", { "presets": ["es2015", "react"] }]]
+  var plugin = [];
+  if (watch) {
+    plugin = [[watchify, {ignoreWatch: ['**/node_modules/**']}]]
+  }
+  var bundler = browserify({
+    entries: "./app/index.jsx",
+    debug: true,
+    cache: {},
+    packageCache: {},
+    plugin: plugin,
+    transform: [["eslintify"], ["babelify", {"presets": ["es2015", "react"]}]]
+  });
+
+  function rebundle() {
+    console.log('-> bundling...');
+    return bundler.bundle()
+      .on('error', function (err) {
+        console.error(err);
+      })
+      .pipe(vinylSourceStream('build.js'))
+      .pipe(vinylBuffer())
+      .pipe(gulpSourcemaps.init({loadMaps: true}))
+      .pipe(gulpSourcemaps.write('./'))
+      .pipe(gulp.dest('./build'));
+  }
+
+  if (watch) {
+    bundler.on('update', function () {
+      rebundle();
     });
-    function rebundle() {
-        console.log('-> bundling...');
-        return bundler.bundle()
-            .on('error', function (err) {
-                console.error(err);
-            })
-            .pipe(vinylSourceStream('build.js'))
-            .pipe(vinylBuffer())
-            .pipe(gulpSourcemaps.init({loadMaps: true}))
-            .pipe(gulpSourcemaps.write('./'))
-            .pipe(gulp.dest('./build'));
-    }
-    if (watch) {
-        bundler.on('update', function () {
-            rebundle();
-        });
-        bundler.on("time", function (time) {
-            console.log("   bundling done in " + time + "ms");
-        });
-    }
-    return rebundle();
-}
-
-// Launch static server
-function browserSync() {
-
+    bundler.on("time", function (time) {
+      console.log("   bundling done in " + time + "ms");
+    });
+  }
+  return rebundle();
 }
 
 // Compile sass files into CSS
 function sass() {
-    return gulp.src("app/assets/sass/main.scss")
-        .pipe(gulpSourcemaps.init())
-        .pipe(gulpSass({
-            outputStyle: "compressed",
-            includePaths : ["node_modules/"]
-        }).on("error", gulpSass.logError))
-        .pipe(gulpAutoprefixer({
-            browsers: ['last 2 versions'],
-            cascade: false
-        }))
-        .pipe(gulpConcat("main.css"))
-        .pipe(gulpSourcemaps.write("./"))
-        .pipe(gulp.dest("build/"));
+  return gulp.src("app/assets/sass/main.scss")
+    .pipe(gulpSourcemaps.init())
+    .pipe(gulpSass({
+      outputStyle: "compressed",
+      includePaths: ["node_modules/"]
+    }).on("error", gulpSass.logError))
+    .pipe(gulpAutoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+    }))
+    .pipe(gulpConcat("main.css"))
+    .pipe(gulpSourcemaps.write("./"))
+    .pipe(gulp.dest("build/"));
 }
 
 gulp.task('build', function () {
-    sass();
-    compile();
+  sass();
+  compile();
 });
 gulp.task('watch', function () {
-    sass();
-    gulp.watch('./app/assets/sass/**/*.scss', sass);
-    compile(true);
+  sass();
+  gulp.watch('./app/assets/sass/**/*.scss', sass);
+  compile(true);
 });
 
 gulp.task('browsersync', function () {
-    // Just want static server, no need for hot reload as of now
-    return browserSync({
-        server: {
-            baseDir: '.'
-        },
-        notify: false
-    });
+  // Just want static server, no need for hot reload as of now
+  return browserSync({
+    server: {
+      baseDir: '.'
+    },
+    notify: false
+  });
 });
 
-gulp.task('default', ['watch', 'browsersync']);
+gulp.task('watchAndLaunch', gulp.parallel('watch', 'browsersync'));
+
+// default task
+gulp.task('default', gulp.series('watchAndLaunch'));
